48f26528934baf042a1ee2b06d4c27b117176f4c
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-08-03 22:01:50 +0800

    ...

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, ForgotPasswordComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatButtonModule],
 })
 export class AuthModule {}
